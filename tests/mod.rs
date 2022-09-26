864ab1c4ba03eead9ca592df28bc8253da084d13
Ygg01 <y.laughing.man.y@gmail.com>
2022-09-26 21:34:15 +0200

    Refactor tests

--- tests/mod.rs
+++ tests/mod.rs
@@ -2,27 +2,30 @@ extern crate steel_yaml;
 
 #[cfg(test)]
 mod tests {
-    use std::fmt::{Debug, format, Write};
+    use std::fmt::{format, Debug, Write};
 
     use steel_yaml::Scanner;
 
+    fn assert_eq_event(input_yaml: &str, expect: &str) {
+        let mut event = String::new();
+        Scanner::from_str_reader(input_yaml).for_each(|x| event.push_str(&*format!("\n{:?}", x)));
+        assert_eq!(expect, event);
+    }
+
     #[test]
     fn parse_empty_document() {
-        let yaml = r#"
+        let input_yaml = r#"
 # test"
   # test
 %YAML 1.3 #arst
 "#;
         let expect = r#"
 +STR
-#TAG 1.3
+#YAML 1.3
 ERR
 +DOC
 -DOC
 -STR"#;
-        let mut event = String::new();
-        Scanner::from_str_reader(yaml)
-            .for_each(|x| event.push_str(&*format!("\n{:?}", x)));
-        assert_eq!(expect, event);
+        assert_eq_event(input_yaml, expect);
     }
 }
