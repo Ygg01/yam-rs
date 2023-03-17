76344253217b284a1ac4a5fc9619ea06d5d4dd6f
Ygg01 <y.laughing.man.y@gmail.com>
2023-03-17 14:17:21 +0100

    Change criterion a bit

--- benches/event_processing.rs
+++ benches/event_processing.rs
@@ -16,7 +16,7 @@ const IN1_EXPECTED: &'static str = r#"
 
 fn bench_str_iter(c: &mut Criterion) {
     let mut group = c.benchmark_group("bench-yaml");
-    group.significance_level(0.1).sample_size(50);
+    group.significance_level(0.05).sample_size(100);
     group.bench_function("bench_yaml", |b| {
         b.iter(|| assert_eq_event(black_box(IN1), black_box(IN1_EXPECTED)))
     });
