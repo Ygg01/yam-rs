72410c32ada502835b9e15356504fb33bb6dfefb
Ygg01 <y.laughing.man.y@gmail.com>
2023-03-17 10:18:15 +0100

    Refactor iterators a bit

--- src/treebuild/iter.rs
+++ src/treebuild/iter.rs
@@ -1,10 +1,37 @@
 use std::collections::HashMap;
+use std::io::BufRead;
+use std::marker::PhantomData;
 
-use crate::{tokenizer::StrReader, Lexer};
+use crate::Lexer;
+use crate::tokenizer::Reader;
 
-use super::{YamlToken};
-pub struct YamlParser<'a, TAG> {
+use super::YamlToken;
+
+pub struct YamlParser<'a, R, B, TAG> {
     pub(crate) spanner: Lexer,
-    pub(crate) reader: StrReader<'a>,
+    pub(crate) reader: R,
     pub(crate) map: HashMap<String, YamlToken<'a, TAG>>,
+    buf: PhantomData<B>,
+}
+
+impl<'a, R, B, TAG> From<&'a str> for YamlParser<'a, R, B, TAG> where R: Reader<()> + From<&'a str> {
+    fn from(value: &'a str) -> Self {
+        YamlParser {
+            spanner: Lexer::default(),
+            reader: From::from(value),
+            map: HashMap::default(),
+            buf: PhantomData::default(),
+        }
+    }
+}
+
+impl<'a, R, B, TAG> From<R> for YamlParser<'a, R, B, TAG> where R: Reader<()> + From<&'a str> {
+    fn from(value: R) -> Self {
+        YamlParser {
+            spanner: Lexer::default(),
+            reader: value,
+            map: HashMap::default(),
+            buf: PhantomData::default(),
+        }
+    }
 }
