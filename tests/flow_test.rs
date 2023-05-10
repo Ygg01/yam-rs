d6b217ef5ec2defa7fde91adc4ad98cc923fdc03
Ygg01 <y.laughing.man.y@gmail.com>
2023-05-11 01:30:53 +0200

    Move set of tests to new file

--- tests/flow_test.rs
+++ tests/flow_test.rs
@@ -2,45 +2,6 @@ mod common;
 
 use crate::common::assert_eq_event;
 
-const EMPTY_DOC_ERR_INPUT: &str = r#"
-# test"
-  # test
-%YAML 1.3 #arst
-"#;
-const EMPTY_DOC_ERR_EVENTS: &str = r#"
- %YAML 1.3
- ERR"#;
-
-const EMPTY_DOC_INPUT: &str = r#"
-%YAML 1.2
----
-"#;
-const EMPTY_DOC_EVENTS: &str = r#"
- %YAML 1.2
- +DOC ---
- -DOC"#;
-
-#[test]
-fn flow_empty_document() {
-    assert_eq_event(EMPTY_DOC_ERR_INPUT, EMPTY_DOC_ERR_EVENTS);
-    assert_eq_event(EMPTY_DOC_INPUT, EMPTY_DOC_EVENTS);
-}
-
-const ERR_DIRECTIVE_INPUT: &str = r#"
-%YAML 1.2
-...
-"#;
-
-const ERR_DIRECTIVE_EVENTS: &str = r#"
- ERR
- %YAML 1.2"#;
-
-#[test]
-fn flow_err_directive() {
-    assert_eq_event(ERR_DIRECTIVE_INPUT, ERR_DIRECTIVE_EVENTS);
-    assert_eq_event(EMPTY_DOC_INPUT, EMPTY_DOC_EVENTS);
-}
-
 const NULL_YAML_INPUT: &str = r#"
 null
 "#;
