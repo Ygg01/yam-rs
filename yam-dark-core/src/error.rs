bc7c59ed2dc7acf3a9944a7a19fba7a7be4f9b3d
Ygg01 <y.laughing.man.y@gmail.com>
2024-02-13 00:05:37 +0100

    WIP add build_tape

--- yam-dark-core/src/error.rs
+++ yam-dark-core/src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+use std::fmt::{Display, Formatter};
 
 #[derive(Debug, PartialEq)]
 pub enum ErrorType {
@@ -9,3 +11,19 @@ pub struct Error {
     /// Type of error
     error: ErrorType,
 }
+
+impl Error {
+    pub fn generic(t: ErrorType) -> Self {
+        Self {
+            error: t,
+        }
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.error)
+    }
+}
+
+impl std::error::Error for Error {}
