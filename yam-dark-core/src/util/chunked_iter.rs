6be55b82706f872af420aa7687c665fdacbcca1e
Ygg01 <y.laughing.man.y@gmail.com>
2024-04-12 09:51:27 +0200

    Add chunked_iter.rs for iterating over SIMD Lengths

--- yam-dark-core/src/util/chunked_iter.rs
+++ yam-dark-core/src/util/chunked_iter.rs
@@ -18,6 +18,12 @@ impl<'a> Iterator for ChunkyIterator<'a> {
 }
 
 impl<'a> ChunkyIterator<'a> {
+    
+    pub(crate) fn from_bytes(bytes: &[u8]) -> ChunkyIterator {
+        ChunkyIterator {
+            bytes
+        }
+    }
     pub(crate) fn finalize(&self) -> &[u8] {
         self.bytes
     }
@@ -35,4 +41,33 @@ fn test_chunk() {
     assert_eq!(iter.next(), Some(&b));
     assert_eq!(iter.next(), None);
     assert_eq!(iter.finalize(), &[]);
+}
+
+#[test]
+fn test_chunk_rem() {
+    let a = [0u8; 64];
+    let b = [1u8; 64];
+    let mut x = [a, b].concat();
+    x.push(3);
+    let mut iter = ChunkyIterator {
+        bytes: &x,
+    };
+    assert_eq!(iter.next(), Some(&a));
+    assert_eq!(iter.next(), Some(&b));
+    assert_eq!(iter.next(), None);
+    assert_eq!(iter.finalize(), &[3]);
+}
+
+#[test]
+fn test_chunk_rem_minus() {
+    let a = [0u8; 64];
+    let b = [1u8; 64];
+    let mut x = [a, b].concat();
+    x.drain(67..);
+    let mut iter = ChunkyIterator {
+        bytes: &x,
+    };
+    assert_eq!(iter.next(), Some(&a));
+    assert_eq!(iter.next(), None);
+    assert_eq!(iter.finalize(), &[1, 1, 1]);
 }
