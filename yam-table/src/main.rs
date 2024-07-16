c34d74775f4245c3c05398015e9d32e0a7d92507
Ygg01 <y.laughing.man.y@gmail.com>
2024-07-17 01:58:29 +0200

    Update indent table generation.

--- yam-table/src/main.rs
+++ yam-table/src/main.rs
@@ -9,21 +9,25 @@ fn main() -> std::io::Result<()> {
     let mut output = String::new();
     for el in x {
         use std::fmt::Write;
-        write!(&mut output, "{:?}\n", el).expect("Error");
+        writeln!(&mut output, "{:?},", el).expect("Error");
     }
     let file = File::create("foo.txt")?;
     let mut buf_reader = BufWriter::new(file);
-    buf_reader.write(output.as_bytes())?;
+    buf_reader.write_all(output.as_bytes())?;
     Ok(())
 }
 
 fn calculate_indent(mask: u8) -> [u8; 8] {
-    let mut result = [0; 8];
-    let mut last_index = 0usize;
-    for pos in 0u8..8 {
-        if mask & (1 << pos) == 0 {
-            result[last_index] = pos;
-            last_index += 1;
+    let mut result = [0, 1, 2, 3, 4, 5, 6, 7];
+    let mut start_pos = None;
+    for pos in 0usize..8 {
+        if mask & (1 << pos) != 0 && start_pos.is_some() {
+            start_pos = None;
+        }
+        let old_val = result[pos];
+        result[pos] = start_pos.unwrap_or(old_val);
+        if mask & (1 << pos) == 0 && start_pos.is_none() {
+            start_pos = Some(result[pos]);
         }
     }
     result
