72f71df1f87ae9bf982976a8a55c5741ef41888a
Ygg01 <y.laughing.man.y@gmail.com>
2025-01-27 20:59:31 +0100

    Refactor and expand `count_indent_dependent` function.
    Refactor the function to improve handling of space and newline masks and pass the tests.

--- yam-dark-core/src/util/mod.rs
+++ yam-dark-core/src/util/mod.rs
@@ -317,43 +317,86 @@ unsafe fn swizzle_u32x8(input: &mut [u32], swizzle: &[u8; 8]) {
     *input.get_unchecked_mut(7) = *input.get_unchecked(*swizzle.get_unchecked(7) as usize);
 }
 
+///
+/// Counts the indentation dependent on previously calculated byte columns.
+///
+/// # Arguments:
+///
+/// * `newline_mask` - A 64-bit mask representing the position of newline characters.
+/// * `space_mask` - A 64-bit mask representing the position of space characters.
+/// * `prev_iter_char` - A mutable reference to a flag indicating if the previous character was iterated (0 or 1).
+/// * `prev_indent` - A mutable reference to the previous indentation value.
+/// * `byte_cols` - An array representing the column positions of each byte.
+/// * `indents` - A mutable array to store the calculated indentation values.
+///
 #[doc(hidden)]
 pub fn count_indent_dependent(
     newline_mask: u64,
-    whitespace_mask: u64,
+    space_mask: u64,
     prev_iter_char: &mut u8,
     prev_indent: &mut u32,
     byte_cols: &[u32; 64],
     indents: &mut [u32; 64],
 ) {
+    ///
+    /// # Arguments:
+    ///
+    /// * `byte_indents`: mutable vector representing indentation of bytes.
+    /// * `starts`: starting index which to swizzle
+    /// * `spaces_mask`: The bit mask representing spaces.
+    /// * `newline_mask`: The bit mask representing newlines.
+    /// * `prev_iter_char`: A mutable reference whether to continue or restart indentation count. `1` means ignore and `0` means continue.
+    /// * `prev_indent`: A mutable reference to the previous indent value.
+    ///
     #[inline]
     fn swizzle_slice(
         byte_indents: &mut [u32; 64],
         starts: usize,
-        mask: usize,
+        spaces_mask: usize,
+        newline_mask: usize,
         prev_iter_char: &mut u8,
         prev_indent: &mut u32,
     ) {
-        if *prev_iter_char == 0 && mask % 2 == 1 {
+        if *prev_iter_char == 0 {
             byte_indents[starts] = *prev_indent;
         }
         // This is safe because:
         // - INDENT SWIZZLE TABLE is guaranteed to have all swizzle array values
         // - input is always a 64 long slice with start being 56 (56+8 = 64) at most.
         unsafe {
-            let swizzle_vec = INDENT_SWIZZLE_TABLE.get_unchecked(mask);
+            let swizzle_vec = INDENT_SWIZZLE_TABLE.get_unchecked(spaces_mask);
             swizzle_u32x8(&mut byte_indents[starts..starts + 8], swizzle_vec);
         }
-        *prev_iter_char = (mask & (1 << 8)) as u8;
+        *prev_iter_char = ((spaces_mask | newline_mask) & (1 << 7)) as u8;
         *prev_indent = byte_indents[starts + 7];
     }
 
-    let space_start_edge = whitespace_mask & !(whitespace_mask << 1);
-    let space_end_edge = (whitespace_mask & !(whitespace_mask >> 1)) << 1;
-    let after_indent_bits = space_end_edge.wrapping_sub(space_start_edge);
+    let space_start_edge =
+        space_mask & !(space_mask << 1) & (newline_mask << 1) | newline_mask << 1;
+    let mut space_end_mask = !((space_mask & !(space_mask >> 1)) << 1);
+
+    let mut after_indent_bits = space_start_edge & space_end_mask;
+    space_end_mask &= space_end_mask << 1;
+    after_indent_bits |= (after_indent_bits << 1) & space_end_mask;
+
+    space_end_mask &= space_end_mask << 2;
+    after_indent_bits |= (after_indent_bits << 2) & space_end_mask;
+
+    space_end_mask &= space_end_mask << 4;
+    after_indent_bits |= (after_indent_bits << 4) & space_end_mask;
+
+    space_end_mask &= space_end_mask << 8;
+    after_indent_bits |= (after_indent_bits << 8) & space_end_mask;
+
+    space_end_mask &= space_end_mask << 16;
+    after_indent_bits |= (after_indent_bits << 16) & space_end_mask;
+
+    space_end_mask &= space_end_mask << 32;
+    after_indent_bits |= (after_indent_bits << 32) & space_end_mask;
+
     unsafe {
         // Safety INVARIANT:
-        // This is always safe since have same alignment and size
+        // This is always safe since have same alignment and size assuming that both are &[u32; 64]
         //  byte_cols: &[u32; 64],
         //  indents: &mut [u32; 64],
         core::ptr::copy_nonoverlapping(byte_cols.as_ptr(), indents.as_mut_ptr(), 64);
@@ -363,6 +406,7 @@ pub fn count_indent_dependent(
         indents,
         0,
         (after_indent_bits & 0xFF) as usize,
+        (newline_mask & 0xFF) as usize,
         prev_iter_char,
         prev_indent,
     );
@@ -370,7 +414,8 @@ pub fn count_indent_dependent(
     swizzle_slice(
         indents,
         8,
-        ((after_indent_bits & 0xFF00) >> 8) as usize,
+        ((after_indent_bits >> 8) & 0xFF) as usize,
+        ((newline_mask >> 8) & 0xFF) as usize,
         prev_iter_char,
         prev_indent,
     );
@@ -378,7 +423,8 @@ pub fn count_indent_dependent(
     swizzle_slice(
         indents,
         16,
-        ((after_indent_bits & 0xFF_0000) >> 16) as usize,
+        ((after_indent_bits >> 16) & 0xFF) as usize,
+        ((newline_mask >> 16) & 0xFF) as usize,
         prev_iter_char,
         prev_indent,
     );
@@ -386,7 +432,8 @@ pub fn count_indent_dependent(
     swizzle_slice(
         indents,
         24,
-        ((after_indent_bits & 0xFF00_0000) >> 24) as usize,
+        ((after_indent_bits >> 24) & 0xFF) as usize,
+        ((newline_mask >> 24) & 0xFF) as usize,
         prev_iter_char,
         prev_indent,
     );
@@ -394,7 +441,8 @@ pub fn count_indent_dependent(
     swizzle_slice(
         indents,
         32,
-        ((after_indent_bits & 0xFF_0000_0000) >> 32) as usize,
+        ((after_indent_bits >> 32) & 0xFF) as usize,
+        ((newline_mask >> 32) & 0xFF) as usize,
         prev_iter_char,
         prev_indent,
     );
@@ -402,7 +450,8 @@ pub fn count_indent_dependent(
     swizzle_slice(
         indents,
         40,
-        ((after_indent_bits & 0xFF00_0000_0000) >> 40) as usize,
+        ((after_indent_bits >> 40) & 0xFF) as usize,
+        ((newline_mask >> 40) & 0xFF) as usize,
         prev_iter_char,
         prev_indent,
     );
@@ -410,7 +459,8 @@ pub fn count_indent_dependent(
     swizzle_slice(
         indents,
         48,
-        ((after_indent_bits & 0xFF_0000_0000_0000) >> 48) as usize,
+        ((after_indent_bits >> 48) & 0xFF) as usize,
+        ((newline_mask >> 48) & 0xFF) as usize,
         prev_iter_char,
         prev_indent,
     );
@@ -418,7 +468,8 @@ pub fn count_indent_dependent(
     swizzle_slice(
         indents,
         56,
-        ((after_indent_bits & 0xFF00_0000_0000_0000) >> 56) as usize,
+        ((after_indent_bits >> 56) & 0xFF) as usize,
+        ((newline_mask >> 56) & 0xFF) as usize,
         prev_iter_char,
         prev_indent,
     );
@@ -469,7 +520,10 @@ fn test_quick_count() {
         &mut actual_indents,
     );
     assert_eq!(
-        &actual_indents[0..25],
-        &[0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 0]
+        &actual_indents[0..32],
+        &[
+            0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 0, 1, 2, 3, 4,
+            4, 4, 4
+        ]
     );
 }
