38c5703e3a50bf43d444b81016b131fa0f017e21
Ygg01 <y.laughing.man.y@gmail.com>
2025-01-27 21:00:22 +0100

    Enhance ChunkyIterator to handle sub-64-byte chunks
    Updated `ChunkyIterator` to accommodate chunks smaller than 64 bytes by padding them with spaces.

--- yam-dark-core/src/util/chunked_iter.rs
+++ yam-dark-core/src/util/chunked_iter.rs
@@ -1,8 +1,14 @@
 #![allow(unused)]
+
+use alloc::vec::Vec;
+use core::ptr;
 use core::slice::from_raw_parts;
+
+const EMPTY_CHUNK: [u8; 64] = [b' '; 64];
 
 pub struct ChunkyIterator<'a> {
     bytes: &'a [u8],
+    extra_bytes: Vec<u8>,
 }
 
 impl<'a> Iterator for ChunkyIterator<'a> {
@@ -10,32 +16,44 @@ impl<'a> Iterator for ChunkyIterator<'a> {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        if self.bytes.len() < 64 {
-            None
-        } else {
-            let len = self.bytes.len();
-            let ptr = self.bytes.as_ptr();
-            // SAFETY: We manually verified the bounds of the split.
-            let (first, tail) = unsafe {
-                (
-                    from_raw_parts(ptr, 64),
-                    from_raw_parts(ptr.add(64), len - 64),
-                )
-            };
-            self.bytes = tail;
-            // SAFETY: We explicitly check for the correct number of elements,
-            //   and do not let the references outlive the slice.
-            Some(unsafe { &*first.as_ptr().cast::<[u8; 64]>() })
+        match self.bytes.len() {
+            i if i >= 64 => {
+                let len = self.bytes.len();
+                let ptr = self.bytes.as_ptr();
+                // SAFETY: We manually verified the bounds of the split.
+                let (first, tail) = unsafe {
+                    (
+                        from_raw_parts(ptr, 64),
+                        from_raw_parts(ptr.add(64), len - 64),
+                    )
+                };
+                self.bytes = tail;
+                // SAFETY: We explicitly check for the correct number of elements,
+                //   and do not let the references outlive the slice.
+                Some(unsafe { &*first.as_ptr().cast::<[u8; 64]>() })
+            }
+            i if i > 0 && i < 64 => unsafe {
+                self.extra_bytes.set_len(64);
+                ptr::copy_nonoverlapping(EMPTY_CHUNK.as_ptr(), self.extra_bytes.as_mut_ptr(), 64);
+                ptr::copy_nonoverlapping(
+                    self.bytes.as_ptr(),
+                    self.extra_bytes.as_mut_ptr(),
+                    self.bytes.len(),
+                );
+                self.bytes = &[];
+                Some(&*self.extra_bytes.as_ptr().cast::<[u8; 64]>())
+            },
+            _ => None,
         }
     }
 }
 
 impl<'a> ChunkyIterator<'a> {
     pub fn from_bytes(bytes: &[u8]) -> ChunkyIterator {
-        ChunkyIterator { bytes }
-    }
-    pub(crate) fn finalize(&self) -> &[u8] {
-        self.bytes
+        ChunkyIterator {
+            bytes,
+            extra_bytes: Vec::with_capacity(64),
+        }
     }
 }
 
@@ -44,11 +62,10 @@ fn test_chunk() {
     let a = [0u8; 64];
     let b = [1u8; 64];
     let x = [a, b].concat();
-    let mut iter = ChunkyIterator { bytes: &x };
+    let mut iter = ChunkyIterator::from_bytes(&x);
     assert_eq!(iter.next(), Some(&a));
     assert_eq!(iter.next(), Some(&b));
     assert_eq!(iter.next(), None);
-    assert_eq!(iter.finalize(), &[]);
 }
 
 #[test]
@@ -57,11 +74,14 @@ fn test_chunk_rem() {
     let b = [1u8; 64];
     let mut x = [a, b].concat();
     x.push(3);
-    let mut iter = ChunkyIterator { bytes: &x };
+    let mut iter = ChunkyIterator::from_bytes(&x);
     assert_eq!(iter.next(), Some(&a));
     assert_eq!(iter.next(), Some(&b));
+
+    let mut c = [b' '; 64];
+    c[0] = 3;
+    assert_eq!(iter.next(), Some(&c));
     assert_eq!(iter.next(), None);
-    assert_eq!(iter.finalize(), &[3]);
 }
 
 #[test]
@@ -70,8 +90,13 @@ fn test_chunk_rem_minus() {
     let b = [1u8; 64];
     let mut x = [a, b].concat();
     x.drain(67..);
-    let mut iter = ChunkyIterator { bytes: &x };
+    let mut iter = ChunkyIterator::from_bytes(&x);
     assert_eq!(iter.next(), Some(&a));
+
+    let mut c = [b' '; 64];
+    c[0] = 1;
+    c[1] = 1;
+    c[2] = 1;
+    assert_eq!(iter.next(), Some(&c));
     assert_eq!(iter.next(), None);
-    assert_eq!(iter.finalize(), &[1, 1, 1]);
 }
