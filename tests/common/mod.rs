dffc5007e0f10a5639bfc83347735b83c01669d3
Ygg01 <y.laughing.man.y@gmail.com>
2025-01-27 20:52:39 +0100

    Fix indentation

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -6,6 +6,6 @@ pub fn assert_eq_event(input_yaml: &str, expect: &str) {
     let mut event = String::new();
     let scan = EventIterator::new_from_string(input_yaml);
     scan.for_each(|x| event.push_str(x.as_ref()));
-    
+
     assert_eq!(expect, event, "Error in {input_yaml}");
 }
