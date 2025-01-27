74f7c72e93abdeb40f25bb28d2c5bb049d6fa730
Ygg01 <y.laughing.man.y@gmail.com>
2025-01-27 21:00:18 +0100

    Refactor parsing logic and integrate with new tape module
    Replaced the old visitor-based parsing implementation with a new approach utilizing a tape structure for parsing logic.

---
+++ yam-dark-core/src/tape/mod.rs
@@ -0,0 +1,33 @@
+pub enum Node<'input> {
+    /// A string, located inside the input slice
+    String(&'input str),
+    /// A `Map` given the `size` starts here.
+    /// The values are keys and value, alternating.
+    Map {
+        /// Numbers of keys in object
+        len: usize,
+        /// Total number of nodes in object, including sub-elements
+        count: usize,
+    },
+    /// A `Sequence` given size starts here
+    Sequence {
+        /// The number of elements in the array
+        len: usize,
+        /// Total number of nodes in object, including sub-elements.
+        count: usize,
+    },
+    /// A static node that is interned into the tape. It can be directly taken a
+    /// and isn't nested.
+    Static(StaticNode),
+}
+
+pub enum StaticNode {
+    /// The null value
+    Value,
+    /// A boolean value
+    Bool(bool),
+    /// A floating point value
+    F64(f64),
+    /// A signed 64-bit integer.
+    I64(i64),
+}
