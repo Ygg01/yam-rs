77b8e0cf722066415c5512368180780891e6ca2d
Ygg01 <y.laughing.man.y@gmail.com>
2025-01-27 20:59:34 +0100

    Refactor parts

--- yam-table/src/main.rs
+++ yam-table/src/main.rs
@@ -39,44 +39,15 @@ fn print2(input: u8) {
     println!("xxx   = {:#010b}", x);
 }
 
-fn print1(input: u8) {
-    println!("\nin  = {:#010b}", input);
-
-    let start_edge = input & !(input << 1);
-    // println!("se  = {:#010b}", start_edge);
-
-    let start_edge2 = start_edge << 1;
-    // println!("se2 = {:#010b}", start_edge2);
-    //
-    let start_edge3 = start_edge2 ^ input;
-    // println!("se3 = {:#010b}", start_edge3);
-
-    let start_edge4 = start_edge3 & input;
-    // println!("se4 = {:#010b}", start_edge4);
-
-    let end_edge = input & !(input >> 1);
-    // println!("ee  = {:#010b}", end_edge);
-
-    let end_edge2 = end_edge >> 1;
-    // println!("ee2 = {:#010b}", end_edge2);
-
-    let end_edge3 = end_edge2 ^ input;
-    // println!("ee3 = {:#010b}", end_edge3);
-
-    let end_edge4 = end_edge3 & input;
-    // println!("ee4 = {:#010b}", end_edge4);
-
-    println!("fin = {:#010b}", end_edge4 & start_edge4);
-}
-
 fn main() {
     // print3(0b1111);
     // print3(0b1111000);
     // print3(0b0011);
     // print3(0b01111101);
-    print3(0b010111101);
+    // print3(0b010111101);
     print3(0b0111100);
-    print3(0b01100);
+    print3(0b1011111);
+    print3(0b10111);
 }
 
 fn find_odd(bits: u8) -> u8 {
@@ -97,13 +68,15 @@ fn find_odd(bits: u8) -> u8 {
 fn print3(input: u8) {
     println!("\nin    = {:#010b}", input);
 
-    let start_edge = input & (input << 1);
-    let end_edge = input & (input >> 1);
+    let left_pad = input & (input << 1);
+    let right_pad = input & (input >> 1);
 
-    let inn = start_edge & end_edge;
+    let inn = left_pad & right_pad;
     println!("inn   = {:#010b}", inn);
 
-    let out = start_edge | end_edge;
+    let sa = input & !(input << 1);
+    // let ee = input & !(input >>1);
+    let out = (left_pad | right_pad);
     println!("out   = {:#010b}", out);
 
     let odd = find_odd(inn) ^ inn;
@@ -118,6 +91,7 @@ fn print3(input: u8) {
     println!("xx2   = {:#010b}", xxx);
 
     let mut yyy = input ^ out;
+    println!("yyy   = {:#010b}", xxx);
 
     yyy ^= yyy << 1;
     yyy ^= yyy << 2;
