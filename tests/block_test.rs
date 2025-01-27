0143959db93a837255448ae8f880729114d02456
Ygg01 <y.laughing.man.y@gmail.com>
2025-01-27 20:52:35 +0100

    Update test

--- tests/block_test.rs
+++ tests/block_test.rs
@@ -10,6 +10,13 @@ const BLOCK2_INPUT: &'static str = r#"
 - y
 "#;
 
+const BLOCK_EXPECTED: &'static str = r#"
+  +SEQ
+    =VAL x
+    -SEP-
+    =VAL y
+  -SEQ"#;
+
 const BLOCK3_INPUT_ERR: &'static str = r#"
   - x
  - y
@@ -20,17 +27,24 @@ const BLOCK_AS_PLAIN: &'static str = r#"
    - y
 "#;
 
-const BLOCK_EXPECTED: &'static str = r#"
+
+
+const BLOCK_AS_PLAIN_EXPECTED: &'static str = r#"
   +SEQ
-    =VAL x
-    -SEP-
-    =VAL y
+    =VAL x - y
   -SEQ"#;
+
 
 mod common;
 
 #[test]
 pub fn block_seq() {
-    // assert_eq_event(BLOCK1_INPUT, BLOCK_EXPECTED);
+    assert_eq_event(BLOCK1_INPUT, BLOCK_EXPECTED);
     assert_eq_event(BLOCK2_INPUT, BLOCK_EXPECTED);
 }
+
+#[test]
+pub fn block_plain() {
+    assert_eq_event(BLOCK_AS_PLAIN, BLOCK_AS_PLAIN_EXPECTED);
+}
+
