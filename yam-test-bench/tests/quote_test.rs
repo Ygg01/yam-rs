f148f1477464ce1f824624c74e09d8a8d3d786a4
Ygg01 <y.laughing.man.y@gmail.com>
2025-08-09 13:36:29 +0200

    Port quote_test.rs to rstest
    Signed-off-by: Ygg01 <y.laughing.man.y@gmail.com>

--- yam-test-bench/tests/quote_test.rs
+++ yam-test-bench/tests/quote_test.rs
@@ -14,31 +14,30 @@ use yam_test_bench::consts::*;
 // Double quote multiline
 #[case::double_quote_multiline1(DQUOTE_MULTI1_INPUT, DQUOTE_MULTI1_EVENTS)]
 #[case::double_quote_multiline2(DQUOTE_MULTI2_INPUT, DQUOTE_MULTI2_EVENTS)]
-#[case::double_quote_6WPF_snippet(X_6WPF_INPUT, X_6WPF_EVENTS)]
 // Double quote EOF
-// #[case::test_z01(DQUOTE_END_INPUT, DQUOTE_END_EVENTS)]
-// #[case::test_z01(DQUOTE_ERR2_INPUT, DQUOTE_ERR2_EVENTS)]
-// #[case::test_z01(DQUOTE_MISS_EOF_INPUT, DQUOTE_MISS_EOF_EVENTS)]
-// #[case::test_z01(DQUOTE_INDENT_ERR_INPUT, DQUOTE_INDENT_ERR_EVENTS)]
-// #[case::test_z01(DQUOTE_COMMENT_ERR_INPUT, DQUOTE_COMMENT_ERR_EVENTS)]
-//
-// #[case::double_quote_tabs(DQUOTE_LEADING_TAB1_INPUT, DQUOTE_LEADING_TAB_EVENTS)]
-// #[case::double_quote_tabs(DQUOTE_LEADING_TAB2_INPUT, DQUOTE_LEADING_TAB_EVENTS)]
-// #[case::double_quote_tabs(DQUOTE_LEADING_TAB3_INPUT, DQUOTE_LEADING_TAB2_EVENTS)]
-// #[case::double_quote_tabs(DQUOTE_LEADING_TAB4_INPUT, DQUOTE_LEADING_TAB2_EVENTS)]
-// #[case::double_quote_tabs(DQUOTE_LEADING_TAB5_INPUT, DQUOTE_LEADING_TAB2_EVENTS)]
-//
-// #[case(DQUOTE_EMPTY1_INPUT, DQUOTE_EMPTY1_EVENTS)]
-//
-// #[case::double_quote_X1_G4RS(X1_G4RS_INPUT, X1_G4RS_EVENTS)]
-// #[case::double_quote_X1_G4RS(X2_G4RS_INPUT, X1_G4RS_EVENTS)]
-// #[case::double_quote_X1_G4RS(X3_G4RS_INPUT, X3_G4RS_EVENTS)]
-//
-// #[case::test_war(X1_NP9H_INPUT, X1_NP9H_EVENTS)]
-// #[case::test_war(DQUOTE_ESC1_INPUT, DQUOTE_ESC1_EVENTS)]
-// #[case::test_war(DQUOTE_ESC2_INPUT, DQUOTE_ESC2_EVENTS)]
-// #[case::test_war(DQUOTE_STR_ESC1_INPUT, DQUOTE_STR_ESC_EVENTS)]
-
+#[case::double_quote_end_input1(DQUOTE_END_INPUT, DQUOTE_END_EVENTS)]
+#[case::double_quote_end_input2(DQUOTE_ERR2_INPUT, DQUOTE_ERR2_EVENTS)]
+#[case::double_quote_miss_eof(DQUOTE_MISS_EOF_INPUT, DQUOTE_MISS_EOF_EVENTS)]
+#[case::double_quote_indent_err(DQUOTE_INDENT_ERR_INPUT, DQUOTE_INDENT_ERR_EVENTS)]
+#[case::double_quote_comment_err(DQUOTE_COMMENT_ERR_INPUT, DQUOTE_COMMENT_ERR_EVENTS)]
+// Double quote tabs
+#[case::double_quote_tabs1(DQUOTE_LEADING_TAB1_INPUT, DQUOTE_LEADING_TAB_EVENTS)]
+#[case::double_quote_tabs2(DQUOTE_LEADING_TAB2_INPUT, DQUOTE_LEADING_TAB_EVENTS)]
+#[case::double_quote_tabs3(DQUOTE_LEADING_TAB3_INPUT, DQUOTE_LEADING_TAB2_EVENTS)]
+#[case::double_quote_tabs4(DQUOTE_LEADING_TAB4_INPUT, DQUOTE_LEADING_TAB2_EVENTS)]
+#[case::double_quote_tabs5(DQUOTE_LEADING_TAB5_INPUT, DQUOTE_LEADING_TAB2_EVENTS)]
+// Double quote empty
+#[case::double_quote_empty(DQUOTE_EMPTY1_INPUT, DQUOTE_EMPTY1_EVENTS)]
+// Double quote example tests
+#[case::double_quote_x1_g4rs(X1_G4RS_INPUT, X1_G4RS_EVENTS)]
+#[case::double_quote_x2_g4rs(X2_G4RS_INPUT, X1_G4RS_EVENTS)]
+#[case::double_quote_x3_g4rs(X3_G4RS_INPUT, X3_G4RS_EVENTS)]
+#[case::double_quote_6wpf(X_6WPF_INPUT, X_6WPF_EVENTS)]
+#[case::double_quote_np9h(X1_NP9H_INPUT, X1_NP9H_EVENTS)]
+// Double quote escapes
+#[case::test_escape1(DQUOTE_ESC1_INPUT, DQUOTE_ESC1_EVENTS)]
+#[case::test_escape2(DQUOTE_ESC2_INPUT, DQUOTE_ESC2_EVENTS)]
+#[case::test_escape3(DQUOTE_STR_ESC1_INPUT, DQUOTE_STR_ESC_EVENTS)]
 fn run_tests(#[case] input: &str, #[case] expected_events: &str) {
     assert_eq_event(input, expected_events);
 }
