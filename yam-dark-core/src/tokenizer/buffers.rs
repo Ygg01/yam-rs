4116aabcf19c6755546f845d9a5a99b32b161fc5
Ygg01 <y.laughing.man.y@gmail.com>
2025-08-21 13:29:10 +0200

    Minor cleanup and adding docs.

--- yam-dark-core/src/tokenizer/buffers.rs
+++ yam-dark-core/src/tokenizer/buffers.rs
@@ -8,11 +8,31 @@ pub trait YamlBuffer {
     fn append<S>(&mut self, src: &S, mark: &mut Mark);
 }
 
+impl YamlBuffer for () {
+    fn append<'s, S>(&mut self, _src: &S, _mark: &mut Mark) {}
+}
+
+/// Traits for input sources used in yam.rs
+///
+/// It allows abstracting over input strings and buffers
 pub trait YamlSource<'s> {
     unsafe fn get_span_unsafely(&self, span: Mark) -> &'s [u8];
     unsafe fn get_u8_unchecked(&self, pos: usize) -> u8;
-
     fn has_more(&self) -> bool;
+}
+
+impl<'s> YamlSource<'s> for &'s str {
+    unsafe fn get_span_unsafely(&self, span: Mark) -> &'s [u8] {
+        self.get_unchecked(span.start..span.end).as_bytes()
+    }
+
+    unsafe fn get_u8_unchecked(&self, pos: usize) -> u8 {
+        *self.as_bytes().get_unchecked(pos)
+    }
+
+    fn has_more(&self) -> bool {
+        false
+    }
 }
 
 impl<'s> YamlSource<'s> for &'s [u8] {
@@ -27,8 +47,4 @@ impl<'s> YamlSource<'s> for &'s [u8] {
     fn has_more(&self) -> bool {
         false
     }
-}
-
-impl YamlBuffer for () {
-    fn append<'s, S>(&mut self, _src: &S, _mark: &mut Mark) {}
 }
