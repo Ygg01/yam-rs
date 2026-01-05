6c7463a2d9b6a8798c5643fc25f6d82c8c59ec25
Ygg01 <y.laughing.man.y@gmail.com>
2026-01-05 21:24:10 +0100

    Add more benches

--- yam-test-bench/benches/yaml_throughtput.rs
+++ yam-test-bench/benches/yaml_throughtput.rs
@@ -2,29 +2,67 @@ use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 use yam_core::Parser;
 use yam_test_bench::write_str_from_event;
 
-const TEST_YAML: &str = include_str!("nested.yaml");
+const NESTED_YAML: &str = include_str!("nested.yaml");
+
+const BIG_TEXT: &str = include_str!("big_text.yaml");
 const SMALL_OBJECTS_YAML: &str = include_str!("small_objects.yaml");
 
-fn bench(c: &mut Criterion) {
-    let mut group = c.benchmark_group("sample-size-example");
+fn bench_noop(c: &mut Criterion) {
+    let mut group = c.benchmark_group("noop-bench");
+    group.throughput(Throughput::Bytes(1));
+    group.bench_function("noop", |b| b.iter(noop));
+    group.finish();
+}
+
+fn bench_big_text(c: &mut Criterion) {
+    let mut group = c.benchmark_group("big-text");
     // Configure Criterion.rs to detect smaller differences and increase sample size to improve
     // precision and counteract the resulting noise.
-    let mut buff = String::with_capacity(TEST_YAML.len());
+    let mut buff = String::with_capacity(BIG_TEXT.len());
     group
         .sample_size(10)
-        .throughput(Throughput::Bytes(TEST_YAML.len() as u64));
-    group.bench_function("noop", |b| b.iter(noop));
-    group.bench_function("saphyr-nested", |b| {
+        .throughput(Throughput::Bytes(BIG_TEXT.len() as u64));
+    group.bench_function("saphyr", |b| {
         b.iter(|| {
             buff.clear();
-            let mut parser = Parser::new_from_str(TEST_YAML);
+            let mut parser = Parser::new_from_str(BIG_TEXT);
             write_str_from_event(&mut buff, &mut parser, false);
             assert!(!buff.is_empty());
         })
     });
-    let mut buff = String::with_capacity(SMALL_OBJECTS_YAML.len());
 
-    group.bench_function("saphyr-small-obj", |b| {
+    group.finish();
+}
+
+fn bench_nested(c: &mut Criterion) {
+    let mut group = c.benchmark_group("big-nested");
+    // Configure Criterion.rs to detect smaller differences and increase sample size to improve
+    // precision and counteract the resulting noise.
+    let mut buff = String::with_capacity(NESTED_YAML.len());
+    group
+        .sample_size(10)
+        .throughput(Throughput::Bytes(NESTED_YAML.len() as u64));
+    group.bench_function("saphyr", |b| {
+        b.iter(|| {
+            buff.clear();
+            let mut parser = Parser::new_from_str(NESTED_YAML);
+            write_str_from_event(&mut buff, &mut parser, false);
+            assert!(!buff.is_empty());
+        })
+    });
+
+    group.finish();
+}
+
+fn bench_small_object(c: &mut Criterion) {
+    let mut group = c.benchmark_group("small-object");
+    // Configure Criterion.rs to detect smaller differences and increase sample size to improve
+    // precision and counteract the resulting noise.
+    let mut buff = String::with_capacity(SMALL_OBJECTS_YAML.len());
+    group
+        .sample_size(10)
+        .throughput(Throughput::Bytes(SMALL_OBJECTS_YAML.len() as u64));
+    group.bench_function("saphyr", |b| {
         b.iter(|| {
             buff.clear();
             let mut parser = Parser::new_from_str(SMALL_OBJECTS_YAML);
@@ -38,5 +76,11 @@ fn bench(c: &mut Criterion) {
 
 fn noop() {}
 
-criterion_group!(benches, bench);
+criterion_group!(
+    benches,
+    bench_noop,
+    bench_big_text,
+    bench_nested,
+    bench_small_object
+);
 criterion_main!(benches);
